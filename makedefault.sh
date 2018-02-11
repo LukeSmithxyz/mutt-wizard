e79d940a32a1884a1b14c8dab4ec07476f9bf5d6
Luke <luke@lukesmith.xyz>
2018-02-11 16:55:19 -0700

    makedefault fixed

--- makedefault.sh
+++ makedefault.sh
@@ -6,5 +6,5 @@ title=$1
 muttdir="$HOME/.config/mutt/"
 muttdirsed=$(echo $muttdir | sed -e 's/\//\\\//g')
 
-grep "$muttdir"muttrc -e "^source .*accounts.*" >/dev/null && \
-	sed -i "s/^source .*accounts.*/source ${muttdirsed}accounts\/$title.muttrc/g" "$muttdir"muttrc
+grep "$muttdir"personal.muttrc -e "^source .*accounts.*" >/dev/null && \
+	sed -i "s/^source .*accounts.*/source ${muttdirsed}accounts\/$title.muttrc/g" "$muttdir"personal.muttrc

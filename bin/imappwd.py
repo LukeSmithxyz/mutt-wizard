de6bd6e5b0ea2c97b1780789d57cabb54f4ec0d4
Luke Smith <luke@lukesmith.xyz>
2019-02-22 08:15:36 -0500

    gpg autodetermined in python script

--- bin/imappwd.py
+++ bin/imappwd.py
@@ -1,10 +1,15 @@
 import os.path
 import subprocess
+import distutils.spawn
 home = os.path.expanduser("~")
 def mailpasswd(acct):
   acct = os.path.basename(acct)
   path = "%s/.local/share/muttwizard/%s.gpg" % (home,acct)
-  args = ["gpg2", "--use-agent", "--quiet", "--batch", "-d", path]
+  if distutils.spawn.find_executable("gpg"):
+      GPG="gpg"
+  else:
+      GPG="gpg2"
+  args = [GPG, "--use-agent", "--quiet", "--batch", "-d", path]
   try:
     return subprocess.check_output(args).strip().decode('UTF-8')
   except subprocess.CalledProcessError:
